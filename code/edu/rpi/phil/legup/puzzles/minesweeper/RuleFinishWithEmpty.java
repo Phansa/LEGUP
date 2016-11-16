2f44b02114736ba8f634e263ec4478c678f403d1
adeet <adeet@129.161.91.110>
2016-11-16 12:30:23 -0500

    adding serial

--- code/edu/rpi/phil/legup/puzzles/minesweeper/RuleFinishWithEmpty.java
+++ code/edu/rpi/phil/legup/puzzles/minesweeper/RuleFinishWithEmpty.java
@@ -4,6 +4,8 @@ import edu.rpi.phil.legup.PuzzleRule;
 
 public class RuleFinishWithEmpty extends PuzzleRule{
 
+	private static final long serialVersionUID = 464884358611362848L;
+
 	@Override
 	public String getImageName() {
 		return "images/minesweeper/rules/FinishWithEmpty.png"; 
