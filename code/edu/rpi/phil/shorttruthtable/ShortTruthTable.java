6c9219b2251b46298a2dd05961d12b093e9d2cd8
adeet <adeet@MSI.lan>
2016-11-16 21:41:14 -0500

    adding

---
+++ code/edu/rpi/phil/shorttruthtable/ShortTruthTable.java
@@ -0,0 +1,43 @@
+package edu.rpi.phil.shorttruthtable;
+
+import java.util.Map;
+import java.util.Vector;
+
+import edu.rpi.phil.legup.CaseRule;
+import edu.rpi.phil.legup.Contradiction;
+import edu.rpi.phil.legup.PuzzleModule;
+import edu.rpi.phil.legup.PuzzleRule;
+
+public class ShortTruthTable extends PuzzleModule{
+
+	@Override
+	public Map<String, Integer> getSelectableCells() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Integer> getUnselectableCells() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector<PuzzleRule> getRules() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector<Contradiction> getContradictions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector<CaseRule> getCaseRules() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
